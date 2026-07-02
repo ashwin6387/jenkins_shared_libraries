0ca93147eab04d52071413cc2a996088e661a316
ashwin6387 <ashwinvishwa135@gmail.com>
2026-07-03 00:23:14 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,9 @@
 def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([UsernamePassword(credentialId: 'dockercred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]){
-    sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  withCredentials([UsernamePassword(
+    credentialId: 'dockercred', 
+    passwordVariable: 'dockerHubPass', 
+    usernameVariable: 'dockerHubUser')]){
+    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
